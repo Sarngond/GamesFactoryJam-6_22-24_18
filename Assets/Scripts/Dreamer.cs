cd9c76a2ca535886810e121cc59a8b0d0ee0234a
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-06-23 23:56:30 -0500

    Dream Text and Dreamer

---
+++ Assets/Scripts/Dreamer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dreamer : MonoBehaviour {
+
+	private Collider2D myCollider;
+
+	public Text dreamText;
+
+	// Use this for initialization
+	void Start () {
+		myCollider = GetComponent<Collider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DreamTextAppear ();
+	}
+
+	void DreamTextAppear () {
+		if (myCollider.IsTouchingLayers (LayerMask.GetMask ("Player"))) {
+			dreamText.enabled = true;
+			print ("Can dream");
+		} else {
+			dreamText.enabled = false;
+		}
+	}
+}
